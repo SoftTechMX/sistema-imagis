8db9e204edcec701972fa3f5e2e8b8cda9f879af
Bayron Vazquez <bayronnjonathan@gmail.com>
2024-06-11 18:10:03 -0500

    reorganizacion de archivos
    Se reorganizaron los archivos y se implemento el uso del IDE QT Creator

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include "mainwindow.h"
 #include "flogin.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    fLogin w;
-    w.show();
-    return a.exec();
+    QApplication aplicacion(argc, argv);
+
+    fLogin ventanaLogin;
+    ventanaLogin.show();
+
+    return aplicacion.exec();
 }
