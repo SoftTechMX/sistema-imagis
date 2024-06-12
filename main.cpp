e74d54abf4c0ea374a428f1110a5f0f63f695a0a
Bayron Vazquez <bayronnjonathan@gmail.com>
2024-06-11 21:58:23 -0500

    WIP

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication aplicacion(argc, argv);
 
-    MainWindow ventanaLogin;
+    fLogin ventanaLogin;
     ventanaLogin.show();
 
     return aplicacion.exec();
